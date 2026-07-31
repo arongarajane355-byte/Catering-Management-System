7749573b1312c60e7c1a389c0c4c0ed03cdc61a7
Ara Jane Arong <arongarajane355@gmail.com>
2026-07-31 17:27:43 +0800

    del
    del

--- frontend/src/components/ServiceCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function ServiceCard({ service }) {
-  return (
-    <div className="service-card">
-      <h4>{service.name}</h4>
-      <p>{service.description}</p>
-      <span className="service-price">₱{Number(service.price).toLocaleString()}</span>
-    </div>
-  );
-}
